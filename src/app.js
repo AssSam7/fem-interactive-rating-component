a081707c2b2b26f5307346fde5ace4127b51ac5b
Aslam Mohammed <aslam.pm@licious.com>
2023-01-27 11:04:43 +0530

    Connecting the JS

---
+++ src/app.js
@@ -0,0 +1,7 @@
+/* DOM Elements */
+const ratingBtns = document.querySelectorAll(".rating-btn");
+const submitBtn = document.querySelector(".rating-btn-submit");
+
+submitBtn.addEventListener("click", () => {
+  console.log("clicked!");
+});
