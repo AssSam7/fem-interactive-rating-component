db837695105c80c1447537cfb4fcb2a27d243977
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2023-03-03 23:45:57 +0530

    Finishing the functionality

--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 /* DOM Elements */
-const ratingBtns = document.querySelectorAll(".rating-btn");
 const submitBtn = document.querySelector(".rating-btn-submit");
+const ratingInputs = document.getElementsByName("num-rating");
+const ratingCard = document.querySelector(".card");
+
+let rating = "";
+
+function getSuccessHTML() {
+  return `<img
+        src="../images/illustration-thank-you.svg"
+        alt="Thank you for the rating"
+        width="162"
+        height="108"
+      />
+
+      <p class="selected-rating">You selected ${rating} out of 5</p>
+
+      <div class="success-msg-wrapper">
+        <h2>Thank you!</h2>
+        <p>
+          We appreciate you taking the time to give a rating. If you ever need
+          more support, don’t hesitate to get in touch!
+        </p>
+      </div>`;
+}
 
 submitBtn.addEventListener("click", () => {
-  console.log("clicked!");
+  ratingInputs.forEach((input) => {
+    if (input.checked) {
+      rating = input.value;
+    }
+  });
+
+  if (rating > 0) {
+    ratingCard.innerHTML = "";
+    ratingCard.classList.add("card-success");
+
+    ratingCard.innerHTML = getSuccessHTML();
+  }
 });
